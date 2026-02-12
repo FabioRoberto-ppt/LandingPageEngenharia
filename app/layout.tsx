46ea444fc40aaf976dfe366a14913118b07bcaa6
= <=>
2026-02-12 13:29:30 -0300

    mudado o header do site

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Hebert Soares | Engenharia de Luxo",
+  title: "Hebert Soares| Engenheiro Civil ",
   description: "Construções e projetos de alto padrão",
 };
 
